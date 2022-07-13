5a57dac20935100338bd6a074c86d629d9adbebf
Chenyt26 <2396164919@qq.com>
2022-07-13 17:01:32 +0800

    [TicketNo:] [Description:] 统一鉴权action工程初始化
    Match-id-683336a9c010e872dcf3eea918d4d1dcc2f3f0ca

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+    clearMocks: true,
+    restoreMocks: true,
+    moduleFileExtensions: ['js', 'ts'],
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.ts'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    verbose: true,
+};
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import * as core from '@actions/core';
+
+export async function run() {}
+
+run().catch(core.setFailed);
